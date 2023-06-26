06d812f991806d7ee9c69ba0e7f57444c335c2d9
MauroMDU <133882896+MauroMDU@users.noreply.github.com>
2023-06-26 14:06:49 -0300

    Minga 03 (#2)
    * minga-02-database
    
    * fixed

--- Modulo Merm/config/db.js
+++
@@ -1,4 +0,0 @@
-import { connect } from "mongoose";
-connect(process.env.LINK_DB)////conecto con el link de la db guardado en la variable de entorno del archivo
-.then(()=>console.log("connected to data base"))///devuelve una promesa por lo que es necesario configurar
-.catch((error)=>console.log(error))//then y cacht
